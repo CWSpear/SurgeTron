b72364ebe86bdc41b042f83ae171ab24c6bd3b0f
Cameron Spear <cameronspear@gmail.com>
2013-10-18 14:08:09 -0700

    Quotes.

--- implementations/hipchat/config-sample.js
+++ implementations/hipchat/config-sample.js
@@ -7,16 +7,16 @@ var config = {
     // (used for listening)
 
     // your user's ID (gottem from URL above while signed into your account)
-    jid: "USER_ID_GOES_HERE@chat.hipchat.com",
+    jid: 'USER_ID_GOES_HERE@chat.hipchat.com',
 
     // password for your account
-    password: "YOUR_PASSWORD",
+    password: 'YOUR_PASSWORD',
 
     // nickname that you'll be known by while listening
-    room_nick: "YOUR_NAME",
+    room_nick: 'YOUR_NAME',
      
     // use HipChat's web API for posting messages
-    hipchat_apikey: "HIPCHAT_API_KEY",
+    hipchat_apikey: 'HIPCHAT_API_KEY',
     hipchat_defaults: {
         room: passed_in_room || 'DEFAULT_ROOM',
 
@@ -32,9 +32,9 @@ var config = {
 // i.e. if the name of the room is "Surge Consulting Group", that is the ROOM_NAME
 switch(config.hipchat_defaults.room) {
     // set up different rooms to jump in (ROOM_ID is from link above)
-    case 'ROOM_NAME': config.room_jid = "ROOM_ID@conf.hipchat.com"; break;
-    case 'ROOM_NAME': config.room_jid = "ROOM_ID@conf.hipchat.com"; break;
-    case 'ROOM_NAME': config.room_jid = "ROOM_ID@conf.hipchat.com"; break;
+    case 'ROOM_NAME': config.room_jid = 'ROOM_ID@conf.hipchat.com'; break;
+    case 'ROOM_NAME': config.room_jid = 'ROOM_ID@conf.hipchat.com'; break;
+    case 'ROOM_NAME': config.room_jid = 'ROOM_ID@conf.hipchat.com'; break;
     default: console.log('Invalid Room: ' + config.hipchat_defaults.room); process.exit(1);
 }
 
