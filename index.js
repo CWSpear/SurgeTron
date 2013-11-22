b2e6d56c5407771f6b42a3216c6aabd07870f6f5
Cameron Spear <cameronspear@gmail.com>
2013-11-21 21:49:56 -0800

    New and improved CLI WIP.

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
-var util = require('util');
+#!/usr/bin/env node --harmony-generators
 
-if(process.argv.length < 3) {
-    console.log('Invalid number of params.');
-    console.log('node index implementation [params...]');
-    process.exit(1);
-}
+var fs = require('fs');
+var program = require('commander');
+var prompt = require('co-prompt');
+var co = require('co');
 
-try {
-    require('./implementations/' + process.argv[2] + '/server.js');
-} catch(e) {
-    if(e.code === 'MODULE_NOT_FOUND')
-        console.log('Invalid implementation: ' + process.argv[2]);
-    else
-        console.error(e);
-    process.exit(1);
-}
+
+program
+    .version('0.9.0')
+    .option('-i, --instance <name>', 'choose an instance for the given implementation')
+    .usage('[options] <implementation or command>');
+
+program
+    .command('*')
+    .description('run with the given implementation')
+    .action(function (implementation, options) { 
+        var path = './implementations/' + implementation + '/server.js';
+        if (fs.existsSync(path)) require(path);
+        else {
+            console.log('');
+            console.log('  Could not find "' + implementation + '" implementation');
+            console.log('  Use `list` command for supported implementations');
+            console.log('');
+        }
+    });
+
+program
+    .command('list')
+    .description('list supported implementations')
+    .action(function () {
+        // TODO: make this prettier and dynamic
+        console.log('');
+        console.log('  hipchat, irc');
+        console.log('');
+    });
+
+program
+    .command('config')
+    .description('run the config wizard')
+    .action(function () {
+        var config = {};
+        co(function *() {
+            var doHipChat = yield prompt.confirm('Configure HipChat (y/N)? ');
+
+            if(doHipChat) {
+                config.hipchat = {
+                    jid: yield prompt('  HipChat Jabber ID (jid): '),
+                    password: yield prompt.password('  Password: '),
+                    room_nick: yield prompt('  Nickname: '),
+                     
+                    hipchat_apikey: yield prompt('  HipChat API Key: '),
+                    hipchat_defaults: {
+                        room: yield prompt('  Default room: '),
+
+                        from: yield prompt('  Bot\'s name: '),
+                        color: yield prompt('  Bot\'s color: '),
+                        notify: yield prompt.confirm('  Should Notify (y/N)? ')
+                    }
+                };
+            }
+        })(function () {
+            console.log(config);
+            process.exit(0);
+        });
+    });
+
+program.parse(process.argv);
+
+if (!program.args.length) program.help();
