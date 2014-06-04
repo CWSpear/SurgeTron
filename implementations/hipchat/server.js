3a2ef259a6a3e3bba43ceb3feaea8e613783a511
Justen Martin <jmartin@arrowshooterstech.com>
2014-06-04 16:33:18 -0500

    refactored so it can be made into a npm module

--- implementations/hipchat/server.js
+++ implementations/hipchat/server.js
@@ -6,86 +6,88 @@ var _ = require('lodash');
 var HipChatClient = require('node-hipchat');
 
 var bot = require('../../bot/bot.js');
-var config = require('./config.js');
 
-var hipchat = new HipChatClient(config.hipchat_apikey);
-
-var cl = new xmpp.Client({
-    jid: config.jid + '/bot',
-    password: config.password
-});
- 
-// Log all data received
-//cl.on('data', function(d) {
-//    util.log("[data in] " + d);
-//});
- 
-// Once connected, set available presence and join room
-cl.on('online', function() {
-    // util.log("We're online!");
- 
-    // set ourselves as online
-    cl.send(new xmpp.Element('presence', { type: 'available' }).
-        c('show').t('chat')
-     );
+module.exports = function(config){
+    var hipchat = new HipChatClient(config.hipchat_apikey);
     
-    // join room (and request no chat history)
-    cl.send(new xmpp.Element('presence', { to: config.room_jid+'/'+config.room_nick }).
-        c('x', { xmlns: 'http://jabber.org/protocol/muc' })
-    );
+    var cl = new xmpp.Client({
+        jid: config.jid + '/bot',
+        password: config.password
+    });
+     
+    // Log all data received
+    //cl.on('data', function(d) {
+    //    util.log("[data in] " + d);
+    //});
+     
+    // Once connected, set available presence and join room
+    cl.on('online', function() {
+        // util.log("We're online!");
+     
+        // set ourselves as online
+        cl.send(new xmpp.Element('presence', { type: 'available' }).
+            c('show').t('chat')
+         );
+        
+        // join room (and request no chat history)
+        cl.send(new xmpp.Element('presence', { to: config.room_jid+'/'+config.room_nick }).
+            c('x', { xmlns: 'http://jabber.org/protocol/muc' })
+        );
+    
+        // send keepalive data or server will disconnect us after 150s of inactivity
+        setInterval(function() {
+            cl.send(new xmpp.Message({}));
+        }, 2 * 60 * 1000);
+    
+        // recommended, but doesn't seem to work...
+        // cl.connection.socket.setTimeout(0);
+        // cl.connection.socket.setKeepAlive(true, 2 * 60 * 1000);
+    });
+    
+    cl.on('stanza', function(stanza) {
+        // always log error stanzas
+        if (stanza.attrs.type == 'error') {
+            // console.log(util.inspect(stanza, { colors:true, depth:null }));
+            // try {
+            //     if(stanza.getChild('error').attrs['type'] != 'cancel')
+            //         util.log('[error] ' + stanza);
+            // } catch(e) {
+            //     util.log('[error] ' + stanza);
+            // }
+            return;
+        }
+    
+        // ignore everything that isn't a room message
+        if (!stanza.is('message') || stanza.attrs.type != 'groupchat') {
+            // util.log('[info] Not a room message.');
+            return;
+        }
+     
+        var body = stanza.getChild('body');
+        // message without body is probably a topic change
+        if (!body) {
+            // util.log('[info] No body');
+            return;
+        }
+    
+        var text = body.getText();
+    
+        var from = stanza.attrs.from.replace(config.room_jid + '/', '');
+        // util.log('[info] (' + from + ') ' + text);
+    
+        if(from == 'SurgeTron') {
+            // util.log('[info] skip message from bot');
+            return;
+        }
+    
+        // bot magic
+        var promise = bot.getCommandPromise(text);
+        if(promise) {
+            promise.then(function(message) {
+                var params = _.defaults({ message: message }, config.hipchat_defaults);
+                hipchat.postMessage(params);
+            });
+        }
+    });
+}
 
-    // send keepalive data or server will disconnect us after 150s of inactivity
-    setInterval(function() {
-        cl.send(new xmpp.Message({}));
-    }, 2 * 60 * 1000);
-
-    // recommended, but doesn't seem to work...
-    // cl.connection.socket.setTimeout(0);
-    // cl.connection.socket.setKeepAlive(true, 2 * 60 * 1000);
-});
-
-cl.on('stanza', function(stanza) {
-    // always log error stanzas
-    if (stanza.attrs.type == 'error') {
-        // console.log(util.inspect(stanza, { colors:true, depth:null }));
-        // try {
-        //     if(stanza.getChild('error').attrs['type'] != 'cancel')
-        //         util.log('[error] ' + stanza);
-        // } catch(e) {
-        //     util.log('[error] ' + stanza);
-        // }
-        return;
-    }
-
-    // ignore everything that isn't a room message
-    if (!stanza.is('message') || stanza.attrs.type != 'groupchat') {
-        // util.log('[info] Not a room message.');
-        return;
-    }
- 
-    var body = stanza.getChild('body');
-    // message without body is probably a topic change
-    if (!body) {
-        // util.log('[info] No body');
-        return;
-    }
-
-    var text = body.getText();
-
-    var from = stanza.attrs.from.replace(config.room_jid + '/', '');
-    // util.log('[info] (' + from + ') ' + text);
-
-    if(from == 'SurgeTron') {
-        // util.log('[info] skip message from bot');
-        return;
-    }
-
-    // bot magic
-    var promise = bot.getCommandPromise(text);
-    if(promise) {
-        promise.then(function(message) {
-            var params = _.defaults({ message: message }, config.hipchat_defaults);
-            hipchat.postMessage(params);
-        });
-    }
-});
--- implementations/irc/server.js
+++ implementations/irc/server.js
@@ -1,26 +1,27 @@
-var config = require('./config');
 var bot = require('../../bot/bot.js');
 var irc = require('irc');
 require('irc-colors').global();
 
-var client = new irc.Client(config.server, config.botName, {
-    channels: config.channels
-});
-
-var augmentText = function(text) {
-    text = text.replace(/<strong[^>]*?>(.*?)<\/strong>/g, '$1'.irc.bold());
-    text = text.replace(/<em[^>]*?>(.*?)<\/em>/g, '$1'.irc.italic());
-    text = text.replace(/<a .*?href="(.*)".*?>(.*?)<\/a>/g, '$2 ($1)');
-    text = text.replace(/<br ?\/?>/g, "\n");
-
-    return text;
-};
-
-client.addListener('message', function(from, to, text, message) {
-    var promise = bot.getCommandPromise(text);
-    if(promise) {
-        promise.then(function(message) {
-            client.say(to == config.botName ? from : to, augmentText(message));
-        });
-    }
-});
+module.exports = function(config) {
+    var client = new irc.Client(config.server, config.botName, {
+        channels: config.channels
+    });
+    
+    var augmentText = function(text) {
+        text = text.replace(/<strong[^>]*?>(.*?)<\/strong>/g, '$1'.irc.bold());
+        text = text.replace(/<em[^>]*?>(.*?)<\/em>/g, '$1'.irc.italic());
+        text = text.replace(/<a .*?href="(.*)".*?>(.*?)<\/a>/g, '$2 ($1)');
+        text = text.replace(/<br ?\/?>/g, "\n");
+    
+        return text;
+    };
+    
+    client.addListener('message', function(from, to, text, message) {
+        var promise = bot.getCommandPromise(text);
+        if(promise) {
+            promise.then(function(message) {
+                client.say(to == config.botName ? from : to, augmentText(message));
+            });
+        }
+    });
+}
